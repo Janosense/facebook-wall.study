030363202d570726eef6986c250c61023b269d29
Janosense <janosense@gmail.com>
2018-11-05 20:47:54 +0200

    Added code from lesson 5.

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react';
 import Composer from '../Composer';
 import Post from '../Post';
-import Styles from './styles.m.css';
 import StatusBar from '../StatusBar';
+import Spinner from '../Spinner';
+import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            {
+                id:      '1',
+                comment: 'Hi, guys!',
+                created: 1526825076849,
+            },
+            {
+                id:      '2',
+                comment: 'Hi, Ukraine!',
+                created: 1526825076849,
+            },
+        ],
+        isPostFetching: true,
+    };
+
     render() {
+        const { posts, isPostFetching } = this.state;
+        const postsJSX = posts.map((post) => {
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning = { isPostFetching } />
                 <StatusBar />
                 <Composer />
-                <Post />
+                { postsJSX }
             </section>
         );
     }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -2,17 +2,25 @@ import React, {Component} from 'react';
 import moment from 'moment';
 import Styles from './styles.m.css';
 import {Consumer} from './../HOC/withProfile';
+import PropTypes from 'prop-types';
 
 export default class Post extends Component {
+    static propTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired,
+    };
+
     render() {
+        const { comment, created } = this.props;
+
         return (
             <Consumer>
                 {(context) => (
                     <section className = { Styles.post }>
                         <img src = { context.avatar }/>
                         <a href = ''>{ `${context.currentUserFirstName} ${context.currentUserLastName}` }</a>
-                        <time>{ moment().format('MMMM DD YYYY') }</time>
-                        <p>Howdy!</p>
+                        <time>{ moment.unix(created).format('MMMM D h:mm:ss a') }</time>
+                        <p>{ comment }</p>
                     </section>
                 )}
             </Consumer>
